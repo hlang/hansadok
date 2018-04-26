050c19ea70026cb5dd4ff9014741aee691c83406
Hartmut Lang <hartmut.lang@gmail.com>
2018-04-26 17:01:20 +0200

    Hansadok first commit

---
+++ src/main/java/de/hartmut/lang/hansadok/HansadokApplication.java
@@ -0,0 +1,12 @@
+package de.hartmut.lang.hansadok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HansadokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HansadokApplication.class, args);
+    }
+}
